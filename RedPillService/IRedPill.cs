b08a9037470f683855c7db2c0e2ccdd85f0af717
mshenoy83 <mshenoy83@gmail.com>
2014-08-04 19:53:39 +1000

    First Check In

---
+++ RedPillService/IRedPill.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+
+namespace RedPillService
+{
+    [ServiceContract(Namespace = "http://KnockKnock.readify.net")]
+    public interface IRedPill
+    {
+        [OperationContract]
+        [FaultContract(typeof(ArgumentOutOfRangeException))]
+        long FibonacciNumber(long n);
+
+        [OperationContract]
+        [FaultContract(typeof(ArgumentNullException))]
+        string ReverseWords(String s);
+
+        [OperationContract]
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+        [OperationContract]
+        ContactDetails WhoAreYou();
+
+    }
+
+    [DataContract(Namespace = "http://KnockKnock.readify.net")]
+    public enum TriangleType
+    {
+        [EnumMember]
+        Error = 0,
+        [EnumMember]
+        Equilateral = 1,
+        [EnumMember]
+        Isosceles = 2,
+        [EnumMember]
+        Scalene = 3
+    }
+
+    [DataContract(Namespace = "http://KnockKnock.readify.net")]
+    public class ContactDetails : INotifyPropertyChanged, IExtensibleDataObject
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if ((propertyChanged != null))
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private ExtensionDataObject extensionData;
+
+        [Browsable(false)]
+        public ExtensionDataObject ExtensionData
+        {
+            get { return extensionData; }
+            set { extensionData = value; }
+        }
+
+        private string EmailAddressField;
+
+        [DataMember]
+        public string EmailAddress
+        {
+            get { return EmailAddressField; }
+            set
+            {
+                if ((object.ReferenceEquals(this.EmailAddressField, value) != true))
+                {
+                    this.EmailAddressField = value;
+                    this.RaisePropertyChanged("EmailAddress");
+                }
+            }
+        }
+
+        private String FamilyNameField;
+
+        [DataMember]
+        public String FamilyName
+        {
+            get { return FamilyNameField; }
+            set
+            {
+                if ((object.ReferenceEquals(this.FamilyNameField, value) != true))
+                {
+                    this.FamilyNameField = value;
+                    this.RaisePropertyChanged("FamilyName");
+                }
+            }
+        }
+
+        private string GivenNameField;
+        [DataMember]
+        public string GivenName
+        {
+            get { return GivenNameField; }
+            set
+            {
+                if ((object.ReferenceEquals(this.GivenNameField, value) != true))
+                {
+                    this.GivenNameField = value;
+                    this.RaisePropertyChanged("GivenName");
+                }
+            }
+        }
+
+        private String PhoneNumberField;
+        [DataMember]
+        public String PhoneNumber
+        {
+            get { return PhoneNumberField; }
+            set
+            {
+                if ((object.ReferenceEquals(this.PhoneNumberField, value) != true))
+                {
+                    this.PhoneNumberField = value;
+                    this.RaisePropertyChanged("PhoneNumber");
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ RedPillService/RedPill.svc.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedPillService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(Namespace = "http://KnockKnock.readify.net")]
+    public class RedPill : IRedPill
+    {
+
+        public long FibonacciNumber(long n)
+        {
+            long a = 0;
+            long b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            if (n > 92)
+            {
+                var exception = new ArgumentOutOfRangeException();
+                throw new FaultException<ArgumentOutOfRangeException>(exception, "Fib(>92) will cause a 64-bit integer overflow.");
+            }
+            for (long i = 0; i < n; i++)
+            {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        
+
+        public string ReverseWords(string s)
+        {
+            string[] strArray = s.Split(' ');
+            StringBuilder strBuild = new StringBuilder();
+            string sep = "";
+            for (var i =0;i<strArray.Length;i++)
+            {
+                var rev = String.Join("",strArray[i].ToCharArray().Reverse());
+                strBuild.Append(sep).Append(rev);
+                sep = " ";
+            }
+            return strBuild.ToString();
+           
+        }
+
+        
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            try
+            {
+                if (a == 1 || b == 1 || c == 1)
+                {
+                    return TriangleType.Error;
+                }
+
+                int iEqualSides = 0;
+                if (a == b)
+                {
+                    iEqualSides++;
+                }
+                if (a == c)
+                {
+                    iEqualSides++;
+                }
+
+                switch (iEqualSides)
+                {
+                    case 1:
+                        return TriangleType.Isosceles;
+                    case 2:
+                        return TriangleType.Equilateral;
+                    default:
+                        return TriangleType.Scalene;
+                }
+            }
+            catch
+            {
+                return TriangleType.Error;
+            }
+        }
+
+      
+
+
+        public ContactDetails WhoAreYou()
+        {
+            return new ContactDetails
+            {
+                EmailAddress = @"mshenoy83@gmail.com",
+                FamilyName = "Shenoy",
+                GivenName = "Madhav",
+                PhoneNumber = "+61422027397"
+            };
+        }
+
+
+        
+    }
+}
